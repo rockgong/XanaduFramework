1281dd3def724179eced35ef0de2c0f253197e8f
Rock Gong <rockgong1987@sina.com>
2017-07-02 14:07:44 +0800

    Add json test code

---
+++ Assets/Script/Test/TestJson.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+public class TestJson : MonoBehaviour {
+	private string _jsonFilePath = "CommonEvent/TestCommonEvent";
+	private JsonData _jsonData = null;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI()
+	{
+		_jsonFilePath = GUILayout.TextField(_jsonFilePath);
+		if (GUILayout.Button("GO"))
+		{
+			TextAsset asset = Resources.Load<TextAsset>(_jsonFilePath);
+			_jsonData = JsonMapper.ToObject(asset.text);
+		}
+		if (_jsonData != null)
+		{
+			GUILayout.Label(_jsonData.ToJson());
+		}
+	}
+}
