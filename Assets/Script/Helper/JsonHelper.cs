c2ff4b7b827be9c1b70ad620ef2c512e5d0486bd
Rock Gong <rockgong1987@sina.com>
2017-07-07 17:53:55 +0800

    Add Json normal parser with reflection

---
+++ Assets/Script/Helper/JsonHelper.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using MainGame;
+using LitJson;
+
+namespace Helper
+{
+	public static class JsonHelper
+	{
+		public static object PolymorphReflectParse(JsonData jsonData, System.Type type)
+		{
+			if (type == typeof(int))
+			{
+				if (jsonData.IsInt)
+					return (int)jsonData;
+				else
+					throw new System.FormatException("Json Format Error : not int");
+			}
+			else if (type == typeof(string))
+			{
+				if (jsonData.IsString)
+					return (string)jsonData;
+				else
+					throw new System.FormatException("Json Format Error : not string");
+			}
+			else if (type == typeof(double))
+			{
+				if (jsonData.IsDouble)
+					return (double)jsonData;
+				else
+					throw new System.FormatException("Json Format Error : not double");
+			}
+			else if (type == typeof(bool))
+			{
+				if (jsonData.IsBoolean)
+					return (bool)jsonData;
+				else
+					throw new System.FormatException("Json Format Error : not boolean");
+			}
+			else if (type.GetElementType() != null)
+			{
+				if (jsonData.IsArray)
+				{
+					int elementCount = jsonData.Count;
+					System.Array result = System.Array.CreateInstance(type.GetElementType(), new int[]{elementCount});
+					for (int i = 0; i < elementCount; i++)
+					{
+						object elem = PolymorphReflectParse(jsonData[i], type.GetElementType());
+						result.SetValue(elem, new int[]{i});
+					}
+					return result;
+				}
+				else
+					throw new System.FormatException("Json Format Error : not array");
+			}
+			else
+			{
+				FieldInfo[] fields = type.GetFields();
+				object result = System.Activator.CreateInstance(type);
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    try
+                    {
+                        fields[i].SetValue(result, PolymorphReflectParse(jsonData[fields[i].Name], fields[i].FieldType));
+                    }
+                    catch(System.Collections.Generic.KeyNotFoundException ex)
+                    {
+                        continue;
+                    }
+                }
+				return result;
+			}
+
+			return null;
+		}
+	}
+}
--- Assets/Script/Test/TestJson.cs
+++ Assets/Script/Test/TestJson.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using LitJson;
+using Helper;
+
+public class NestedConfig
+{
+    public int nestedIField = 0;
+    public string nestedSField = string.Empty;
+}
+
+public class TestConfig
+{
+    public int iField = 0;
+    public string sField = string.Empty;
+    public int[] aField = new int[0];
+    public NestedConfig nestedConfig = null;
+    public NestedConfig[] nestedConfigArray = null;
+
+    public override string ToString()
+    {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        System.Reflection.FieldInfo[] fields = this.GetType().GetFields();
+        for (int i = 0; i < fields.Length; i++)
+        {
+            sb.Append(fields[i].Name);
+            sb.Append('=');
+            sb.Append(fields[i].GetValue(this).ToString());
+            sb.Append('\n');
+        }
+        return sb.ToString();
+    }
+}
 
 public class TestJson : MonoBehaviour {
 	private string _jsonFilePath = "CommonEvent/TestCommonEvent";
 	private JsonData _jsonData = null;
+    private TestConfig _testConfig = null;
 	// Use this for initialization
 	void Start () {
 		
@@ -23,10 +54,14 @@ public class TestJson : MonoBehaviour {
 		{
 			TextAsset asset = Resources.Load<TextAsset>(_jsonFilePath);
 			_jsonData = JsonMapper.ToObject(asset.text);
+            _testConfig = Helper.JsonHelper.PolymorphReflectParse(_jsonData, typeof(TestConfig)) as TestConfig;
 		}
 		if (_jsonData != null)
 		{
-			GUILayout.Label(_jsonData.ToJson());
+			if (_testConfig != null)
+            {
+                GUILayout.Label(_testConfig.ToString());
+            }
 		}
 	}
 }
