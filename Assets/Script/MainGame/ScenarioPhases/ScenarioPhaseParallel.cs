7cd9b88aca0b7d934fdf302c971306bcd4c41e31
Rock Gong <rockgong1987@sina.com>
2017-07-11 10:54:50 +0800

    Add serial and parallel scenario phase

---
+++ Assets/Script/MainGame/ScenarioPhases/ScenarioPhaseParallel.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameKernal;
+
+namespace MainGame
+{
+	public class ScenarioPhaseDataParallel : BaseScenarioPhaseData
+	{
+		public BaseScenarioPhaseData[] members;
+	}
+
+	class ScenarioPhaseParallel : BaseScenarioPhase
+	{
+		public BaseScenarioPhase[] members;
+
+		public override void Setup(IGameKernal kernal, MonoScenarioScene scene)
+		{
+			base.Setup(kernal, scene);
+
+			if (members != null)
+			{
+				for (int i = 0; i < members.Length; i++)
+					members[i].Setup(kernal, scene);
+			}
+		}
+
+		public override void Enter()
+		{
+			if (members != null)
+			{
+				for (int i = 0; i < members.Length; i++)
+					members[i].Enter();
+			}
+		}
+
+		public override bool CheckOver()
+		{
+			bool result = true;
+
+			if (members != null)
+			{
+				for (int i = 0; i < members.Length; i++)
+				{
+					result &= members[i].CheckOver();
+				}
+			}
+
+			return result;
+		}
+
+		public override void Exit()
+		{
+			if (members != null)
+			{
+				for (int i = 0; i < members.Length; i++)
+				{
+					members[i].Exit();
+				}
+			}
+
+			return ;
+		}
+
+		public static BaseScenarioPhase BuildHandler(BaseScenarioPhaseData data, ScenarioPhaseBuilder builder)
+		{
+			ScenarioPhaseDataParallel target = (ScenarioPhaseDataParallel)data;
+			ScenarioPhaseParallel result = new ScenarioPhaseParallel();
+
+			if (target != null)
+			{
+				result.members = new BaseScenarioPhase[target.members.Length];
+				for (int i = 0; i > target.members.Length; i++)
+					result.members[i] = builder.Build(target.members[i]);
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Assets/Script/MainGame/ScenarioPhases/ScenarioPhaseSerial.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameKernal;
+
+namespace MainGame
+{
+	public class ScenarioPhaseDataSerial : BaseScenarioPhaseData
+	{
+		public BaseScenarioPhaseData[] members;
+	}
+
+	class ScenarioPhaseSerial : BaseScenarioPhase
+	{
+		public BaseScenarioPhase[] members;
+
+		private int _curIndex = 0;
+		private bool _firstCheck = true;
+
+		public override void Setup(IGameKernal kernal, MonoScenarioScene scene)
+		{
+			base.Setup(kernal, scene);
+
+			if (members != null)
+			{
+				for (int i = 0; i < members.Length; i++)
+					members[i].Setup(kernal, scene);
+			}
+		}
+
+		public override void Enter()
+		{
+			_curIndex = 0;
+			if (members != null && members.Length > 0)
+				members[0].Enter();
+
+			return;
+		}
+
+		public override bool CheckOver()
+		{
+			if (members != null && _curIndex < members.Length)
+			{
+				if (members[_curIndex].CheckOver())
+				{
+					members[_curIndex].Exit();
+					_curIndex++;
+					if (_curIndex < members.Length)
+						members[_curIndex].Enter();
+				}
+
+				return false;
+			}
+
+			return true;
+		}
+
+		public override void Exit()
+		{
+			return;
+		}
+
+		public static BaseScenarioPhase BuildHandler(BaseScenarioPhaseData data, ScenarioPhaseBuilder builder)
+		{
+			ScenarioPhaseDataSerial target = (ScenarioPhaseDataSerial)data;
+			ScenarioPhaseSerial result = new ScenarioPhaseSerial();
+
+			if (target != null)
+			{
+				result.members = new BaseScenarioPhase[target.members.Length];
+				for (int i = 0; i > target.members.Length; i++)
+					result.members[i] = builder.Build(target.members[i]);
+			}
+
+			return result;
+		}
+	}
+}
