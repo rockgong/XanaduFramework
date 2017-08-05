8d6fad3f6913e3ec7ebdb26c837759d718cde279
Gong Yan <15800301780@163.com>
2017-08-05 18:00:11 +0800

    Add stage animation scenario phase and adjust scenario phase setting

---
+++ Assets/Script/MainGame/ScenarioPhases/ScenarioPhaseStageAnimation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameKernal;
+using Helper;
+
+namespace MainGame
+{
+	public class ScenarioPhaseDataStageAnimatin : BaseScenarioPhaseData
+	{
+		public string targetName;
+		public string animationName;
+	}
+
+	class ScenarioPhaseStageAnimatin : BaseScenarioPhase
+	{
+		public string targetName;
+		public string animationName;
+
+		public override void Enter()
+		{
+			IStage stage = _gameKernal.GetStage();
+			if (stage != null && !string.IsNullOrEmpty(targetName) && !string.IsNullOrEmpty(animationName))
+			{
+				stage.PlayerStageAnimation(targetName, animationName);
+			}
+		}
+
+		public override bool CheckOver()
+		{
+			return true;
+		}
+
+		public override void Exit()
+		{
+			return;
+		}
+
+		public static BaseScenarioPhase BuildHandler(BaseScenarioPhaseData data, ScenarioPhaseBuilder builder)
+		{
+			ScenarioPhaseDataStageAnimatin target = (ScenarioPhaseDataStageAnimatin)data;
+			ScenarioPhaseStageAnimatin result = new ScenarioPhaseStageAnimatin();
+
+			result.targetName = target.targetName;
+			result.animationName = target.animationName;
+
+			return result;
+		}
+	}
+}
