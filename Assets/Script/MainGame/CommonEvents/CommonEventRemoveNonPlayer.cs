d41456d14eca807b2e49f22002c9b835e882bcfa
Gong Yan <15800301780@163.com>
2017-08-23 12:52:44 +0800

    Handle final door error

---
+++ Assets/Script/MainGame/CommonEvents/CommonEventRemoveNonPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameKernal;
+
+namespace MainGame
+{
+	public class CommonEventRemoveNonPlayer : BaseCommonEvent
+	{
+		public string nonPlayerName;
+	}
+
+	class MainGameCommandRemoveNonPlayer : BaseMainGameCommand
+	{
+		public string nonPlayerName;
+
+		public override void Excute(MainGameCommandManager mgr)
+		{
+			IGameKernal kernal = mgr.gameKernal;
+			if (kernal != null)
+				kernal.RemoveNonPlayerCharacter(nonPlayerName);
+		}
+
+		public static BaseMainGameCommand BuildHandler(BaseCommonEvent evt, MainGameCommandBuilder builder)
+		{
+			CommonEventRemoveNonPlayer targetEvt = (CommonEventRemoveNonPlayer)evt;
+
+			MainGameCommandRemoveNonPlayer result = new MainGameCommandRemoveNonPlayer();
+			result.nonPlayerName = targetEvt.nonPlayerName;
+
+			return result;
+		}
+	}
+}
